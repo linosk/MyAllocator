ba60dbe944c8380b5bf1eb46e0d1c1431d87a12b
linosk <linakoskar@gmail.com>
2022-10-31 18:58:53 +0100

    fprintf over printf

--- main.c
+++ main.c
@@ -22,11 +22,15 @@ int main(int argc, char** argv)
 {
 
 	//CHECK COMPILED PROGRAM WITH ONLY PRINTF WITH VALGRIND DO SEE THAT PRINTF DOES ALLOCATES 1024 BYTS!!!!
-
+/*
     Node* head = NULL;
     insert(head,1);
 
     print_chunks();
+*/ 
+	//FPRINTF DOES NOT ALLOCETS ANY BYTES !!!
+
+	fprintf(stderr,"FGFDGFSGD\n");
 
     return 0;
 }
